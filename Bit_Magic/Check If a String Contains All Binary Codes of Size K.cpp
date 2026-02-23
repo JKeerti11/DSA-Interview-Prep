2fc63d7042e563965335b663ae8bd8c4efd27838
MAZHAR <iiestshibpur605@gmail.com>
2026-02-23 06:50:35 +0530

    Added YouTube Link

--- Bit_Magic/Check If a String Contains All Binary Codes of Size K.cpp
+++ Bit_Magic/Check If a String Contains All Binary Codes of Size K.cpp
@@ -1,6 +1,6 @@
 /*   Scroll below to see JAVA code also  */
 /*
-    MY YOUTUBE VIDEO ON THIS Qn : 
+    MY YOUTUBE VIDEO ON THIS Qn : https://www.youtube.com/watch?v=-ShPKo-FXbw
     Company Tags                : GOOGLE
     Leetcode Link               : https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
 */
